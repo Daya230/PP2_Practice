0c6ef5bad668d6a296e19e17517e54ffad9fd5a2
Dayana <dutpaevad@gmail.com>
2026-01-30 23:52:22 +0500

    Add Practise1 folder

---
+++ Practise1/comments.py
@@ -0,0 +1,32 @@
+#ex1
+# This line prints a message on the screen Пишет сообщение на экран
+print("Hello, World!")
+
+#ex2
+print("Hello, World!")  # This is a greeting это приветствие
+
+#ex3
+# Присваиваем значение
+# Выводим
+# Просто и легко
+
+x = 10
+print(x)
+
+#ex4
+
+# We use 10 because it is the starting value
+x = 10
+x = x + 5
+
+print(x)
+#ex5
+"""
+¨/\_/\ ♥
+>^,^<
+’¨(__)__
+"""
+print("This is cat")
+
+
+
---
+++ Practise1/output.py
@@ -0,0 +1,21 @@
+#ex1
+print("Hello World!")
+
+#ex2
+print(34545)
+print(3 + 3)
+print(2 * 5)
+print(10 - 4)
+print(8 / 2)
+
+#ex3
+print("This will work!")
+print('This will also work!')
+
+#ex4
+print("Hello World!", end=" ")
+print("same line.")
+
+#ex5
+print("I am", 35, "years old.")
+print("Result:", 7 + 8)
