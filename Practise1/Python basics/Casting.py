0ac4eb5be7a92aa719b0dbb87121726ca3185167
Daya230 <dutpaevad@gmail.com>
2026-02-01 02:42:17 +0500

    удаление папки

---
+++ Practise1/Python basics/Casting.py
@@ -0,0 +1,31 @@
+#ex1
+a = int(7.9)
+b = int("42")
+print(a, type(a))
+print(b, type(b))
+
+
+#ex2 
+x = float(5)
+y = float("3.14")
+print(x, type(x))
+print(y, type(y))
+
+#ex3
+num1 = str(10)
+num2 = str(2.718)
+print(num1, type(num1))
+print(num2, type(num2))
+
+#ex4
+age = "25"
+an = int(age)
+h = 1.68
+print(an + 5)
+print(h + 0.1)
+
+#ex5
+text = "Python"
+'''
+print(int(text)) автоматически выйдет ошибка
+'''
---
+++ Practise1/Python basics/Strings.py
@@ -0,0 +1,37 @@
+#ex1
+
+word = "banana"
+if "a" in word:
+    print("буква 'a'")
+
+#ex 2 slicing 
+
+text = "Hello, World!"
+print(text[7:12])
+
+#ex3 
+word = "python"
+print(word.upper().replace("O", "0"))
+
+#ex4 
+a = "Hello"
+b = "World"
+print(a + " " + b)
+
+#ex5
+age = 36
+print(f"My name is John, I am {age} years old")
+
+#ex6
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+#ex7
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
+'''
+Вывод будет: 
+    Hello
+'''
