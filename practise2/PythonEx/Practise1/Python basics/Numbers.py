a6aa285c60ef84443bb3c04092b3836a91913fe4
Daya230 <dutpaevad@gmail.com>
2026-02-01 02:32:06 +0500

    Fix: converted embedded repo to normal folder

---
+++ practise2/PythonEx/Practise1/Python basics/Numbers.py
@@ -0,0 +1,28 @@
+#Ex1
+x = 10
+y = -5
+z = 123456
+print("Example 1:", x + y, type(x))
+
+#Ex2
+a = 3.14
+b = -0.5
+c = 2.0
+print("Example 2:", a * c, type(b))
+
+#Ex 3
+p = 2 + 3j
+q = 5j
+r = -1 - 1j
+print("Example 3:", p + q, type(p))
+
+#Ex 4
+x = 5       
+y = 3.2     
+
+print("Example 4:", float(x), int(y), complex(x))
+
+#Ex 5
+import random
+rand_num = random.randrange(1, 10)
+print("Example 5:", rand_num)
