f6fb6961ef720988f50f97985a07d2b3f5dc9cd8
Daya230 <dutpaevad@gmail.com>
2026-02-11 01:46:06 +0500

    examples

--- practise3/ejudge3/303.py
+++ practise3/ejudge3/303.py
@@ -1,9 +1,29 @@
-global numbers
-numbers =("ZER","ONE","TWO","THR","FOU","FIV","SIX","SEV","EIG","NIN")
-operators = ("+", "-", "*")
-def str_to_num(nums):
-    if nums in numbers:
-        return numbers.index(nums)
+numbers = ("ZER","ONE","TWO","THR","FOU","FIV","SIX","SEV","EIG","NIN")
+operators = "+-*"
 
-a = input()
-print(str_to_num(a))
+def str_to_num(s):
+    digits = []
+    for i in range(0, len(s), 3):
+        digits.append(str(numbers.index(s[i:i+3])))
+    return int("".join(digits))
+
+def num_to_str(n):
+    return "".join(numbers[int(d)] for d in str(n))
+
+expr = input()
+
+for op in operators:
+    if op in expr:
+        left, right = expr.split(op)
+        a = str_to_num(left)
+        b = str_to_num(right)
+
+        if op == "+":
+            res = a + b
+        elif op == "-":
+            res = a - b
+        else:
+            res = a * b
+
+        print(num_to_str(res))
+        break
